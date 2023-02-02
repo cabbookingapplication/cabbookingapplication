8a838144991b29e19ebc6c93f7464be863e62175
cabbookingapplication <123923726+cabbookingapplication@users.noreply.github.com>
2023-02-02 23:05:40 +0530

    Add files via upload

---
+++ CabBookingApplication/src/main/java/com/cg/mts/dto/ReviewResponseDTO.java
@@ -0,0 +1,12 @@
+package com.cg.mts.dto;
+
+import lombok.Data;
+
+@Data
+public class ReviewResponseDTO {
+	private String driverName;
+	private int StarRating;
+	private String message;
+	
+
+}
---
+++ CabBookingApplication/src/main/java/com/cg/mts/repository/IReviewRepository.java
@@ -0,0 +1,20 @@
+package com.cg.mts.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.mts.entities.DriverReview;
+@Repository
+
+public interface IReviewRepository extends JpaRepository<DriverReview, Integer>{
+
+	public DriverReview findById (int id);
+
+	public List<DriverReview> getReviewByDriverName(String driverName);
+
+
+}
+
+
